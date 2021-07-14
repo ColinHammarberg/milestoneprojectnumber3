b5f6ca9e11e44aa30d6d5d35b0277b4663c27253
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-07-14 14:47:38 +0000

    Began working on the register page and managed with some help from code institute to make the code in the app.py file work. Now I'm heading towards creating the register an account form on the register.html page/file. Will test everything later today to see if the user gets registered into the mongodb as expected.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Love Therapy
+
 <a href="" target="_blank"><img src="/documentation/Desktop.png" alt="Book Bites mobile Screen"></a>
 <a href="" target="_blank"><img src="/documentation/Ipad.png" alt="Book Bites mobile Screen"></a>
 <a href="" target="_blank"><img src="/documentation/Mobile.png" alt="Book Bites mobile Screen"></a>
--- app.py
+++ app.py
@@ -25,6 +25,33 @@ def love_therapy():
     therapy = list(mongo.db.therapy.find())
     return render_template("lovetherapy.html", therapy=therapy)
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # This check if the user already exists in the databse
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "email": request.form.get("email").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
+
+
+
 
 
 if __name__ == "__main__":
