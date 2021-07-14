6e340b669538a1c10d9d8708d87db95fb201cc41
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-07-14 18:46:50 +0000

    Managed to simply get my swich button to work by just adding a required statement in the end of the label/input field. Probably will add a flash message there as well. I also began building a footer section, but having a hard time getting the accurate placement of it at the momnent. Gotta spend some more time on it later tonight or tomorrow morning.

--- app.py
+++ app.py
@@ -50,9 +50,6 @@ def register():
     return render_template("register.html")
 
 
-
-
-
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
