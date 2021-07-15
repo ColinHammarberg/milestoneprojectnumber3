434f127a70b8304106b5ed6f453fdb0f1bbfbae9
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-07-15 09:06:27 +0000

    Created the functionality for the log in page. With a little bit of help from code institute and previos exercises I got it to work. I also activated the flash messages from the base.html file. Added all of the icons to the login file (the button) as well. Next up is to hide some of the navbar menu selections when the user is not signed in.

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ def love_therapy():
     therapy = list(mongo.db.therapy.find())
     return render_template("lovetherapy.html", therapy=therapy)
 
+        # register function will create a user and add the user details into the database (Mongodb)
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -48,6 +50,36 @@ def register():
         flash("Registration Successful!")
 
     return render_template("register.html")
+
+        # signin function will log in the user if it matches any of the users in the database
+
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    if request.method == "POST":
+        # app will check if username exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "love_therapy", username=session["user"]))
+            else:
+                # Password doesn't match any user in the database
+                flash("Incorrect Username")
+                return redirect(url_for("signin"))
+
+        else:
+            # username doesn't match any user in the database
+            flash("Incorrect Username")
+            return redirect(url_for("signin"))
+
+    return render_template("signin.html")
 
 
 if __name__ == "__main__":
